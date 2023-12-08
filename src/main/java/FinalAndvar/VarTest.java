5d7e7e210070b8dc1b539816bcdec3a11c23ec1e
Qings <2729783997@qq.com>
2023-12-08 17:57:30 +0800

    Var的初次使用

---
+++ src/main/java/FinalAndvar/VarTest.java
@@ -0,0 +1,13 @@
+package FinalAndvar;
+
+import java.awt.image.BufferStrategy;
+
+public class VarTest {
+    public static void main(String[] args) {
+        var s = new StringBuilder();
+        s.append("hello");
+        s.append(" ");
+        s.append("world");
+        System.out.println(s);
+    }
+}
